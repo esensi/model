5c4481e003b9cdb2c66218fadd68c6c75e32650d
dalabarge <daniel@bexarcreative.com>
2015-05-15 18:33:06 -0500

    Bring back watson/validating rulesets from 0.10.8.
    Conflicts:
    	composer.json

--- src/Observers/ValidatingModelObserver.php
+++ src/Observers/ValidatingModelObserver.php
@@ -1,5 +1,6 @@
 <?php namespace Esensi\Model\Observers;
 
+use \Illuminate\Database\Eloquent\Model;
 use \Watson\Validating\ValidatingObserver;
 
 /**
@@ -15,5 +16,76 @@ use \Watson\Validating\ValidatingObserver;
  * @see \Watson\Validating\ValidatingObserver
  */
 class ValidatingModelObserver extends ValidatingObserver {
+
+    /**
+     * Register the validation event for creating the model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return boolean
+     */
+    public function creating(Model $model)
+    {
+        if ($model->getRuleset('creating'))
+        {
+            return $this->performValidation($model, 'creating');
+        }
+    }
+
+    /**
+     * Register the validation event for updating the model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return boolean
+     */
+    public function updating(Model $model)
+    {
+        if ($model->getRuleset('updating'))
+        {
+            return $this->performValidation($model, 'updating');
+        }
+    }
+
+    /**
+     * Register the validation event for saving the model. Saving validation
+     * should only occur if creating and updating validation does not.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return boolean
+     */
+    public function saving(Model $model)
+    {
+        if ( ! $model->getRuleset('creating') && ! $model->getRuleset('updating'))
+        {
+            return $this->performValidation($model, 'saving');
+        }
+    }
+
+    /**
+     * Register the validation event for deleting the model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return boolean
+     */
+    public function deleting(Model $model)
+    {
+        if ($model->getRuleset('deleting'))
+        {
+            return $this->performValidation($model, 'deleting');
+        }
+    }
+
+    /**
+     * Register the validation event for restoring the model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return boolean
+     */
+    public function restoring(Model $model)
+    {
+        if ($model->getRuleset('restoring'))
+        {
+            return $this->performValidation($model, 'restoring');
+        }
+    }
 
 }
